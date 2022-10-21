251b42c280b350ee048854bc391cbeb15c0bb611
BrunoAdaniya <48045008@est.ort.edu.ar>
2022-10-21 18:07:04 -0300

    PREGUNTAR COMO COMPARAR LOS ID DE UNA TABLA CON LA OTRA SIENDO UNO DE TIPO AUTONUMERACION Y OTRO DE TIPO NUMERO

--- WindowsFormsApp2/WindowsFormsApp2/Progreso.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Progreso.Designer.cs
@@ -32,28 +32,27 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(373, 21);
+            this.dataGridView1.Location = new System.Drawing.Point(102, 213);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(352, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(584, 150);
             this.dataGridView1.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(181, 86);
+            this.textBox1.Location = new System.Drawing.Point(322, 108);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(137, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(194, 128);
+            this.button1.Location = new System.Drawing.Point(340, 151);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 2;
@@ -64,34 +63,25 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 89);
+            this.label1.Location = new System.Drawing.Point(202, 111);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(88, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Ingresar Nombre:";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(326, 268);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(155, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Mostrar el id del usuario ingresado";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Progreso
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Progreso";
             this.Text = "Progreso";
+            this.Load += new System.EventHandler(this.Progreso_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -104,6 +94,5 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
     }
 }
